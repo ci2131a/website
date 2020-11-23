2e8a7e1f96916b9e7a8395fc2df37f555c5a7b0b
Charles <ci2131a@american.edu>
2020-11-22 23:01:48 -0500

    change for my logo

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img alt="Charles logo" src="./assets/PL.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
