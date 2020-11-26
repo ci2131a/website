0e746fdda1893e9cb2015cc7b923be5ba8871929
Charles <ci2131a@american.edu>
2020-11-26 13:47:58 -0500

    change color

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #d3d3d3;
   margin-top: 60px;
 }
 </style>
