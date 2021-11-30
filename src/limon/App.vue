190156ee702d60d4d9c06ac3a5dc3686d53f7f5c
Charles <ci2131a@american.edu>
2021-11-30 10:57:22 -0500

    directory for mpa

---
+++ src/limon/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id = "subpage">
+    <div class = "toprow">
+      <div class = "toptext">
+        <h1>Limoncello</h1>
+        <h3>charlesfi@outlook.com</h3>
+        <a href="CV.1.10.2021.pdf">Curriculum Vitae</a>
+        <div class = "icons">
+         <a href="https://www.linkedin.com/in/charles-ingulli/"><img src="./assets/linkedin23px.svg" style="margin:10px 5px 5px 5px; width:23px; height:auto;"/></a>
+         <a href="https://github.com/ci2131a"><img src="./assets/ghicon.svg" style="margin:10px 5px 5px 5px; width:23px; height:auto;"/></a>
+         <img src="./assets/PL23px.svg" style="margin:10px 5px 5px 5px; width:auto; height:23px;">
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+/*<!--import CI from "./components/CI.vue";-->*/
+
+export default {
+  name: "limoncello",
+};
+</script>
+
+<style>
+h1 {
+  font-size: 42px;
+  /*line-height: 20px;*/
+  margin: 0px;
+}
+h3 {
+  font-size: 16px;
+  /*line-height: 3px;*/
+  font-weight:normal; 
+  margin: 0px;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /*text-align: center;*/
+  color:  #000000;
+  margin-top: 60px;
+}
+.toprow {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  vertical-align: middle;
+}
+a {
+  /*color: #42b983;*/
+  color: #ff8000;
+}
+html {
+  background-color: #F0F0F0;
+}
+</style>
+
+<!--  color: #2c3e50; -->
+<!--  background-color: #d3d3d3; -->
