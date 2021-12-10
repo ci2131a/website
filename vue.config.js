1cd47907d979c3218a76a90b80b4793f69253cda
Charles <ci2131a@american.edu>
2021-12-10 12:01:01 -0500

    modify public path

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,10 @@ module.exports = {
 	pages: {
     	index: './src/pages/index/main.js',
     	limon:'./src/pages/limon/main.js'
-  }
+  }, 
+  publicPath: 
+    process.env.NODE_ENV === 'production'
+    ? '/website/'
+    : '/'
+
 }
