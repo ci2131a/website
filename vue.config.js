bf87f3a5568c893b0dbb3ab716fc836f076dd297
Charles Ingulli <charlesfi@outlook.com>
2024-01-18 23:51:18 -0500

    update config files

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,11 @@
 module.exports = {
 	pages: {
     	index: './src/pages/index/main.js',
-    	limoncello:'./src/pages/limoncello/main.js'
+    	limoncello:'./src/pages/limoncello/main.js',
+    	wlsd:'./src/pages/wlsd/main.js',
+    	lbp:'./src/pages/lbp/main.js',
+      	faceshield: './src/pages/faceshield/main.js',
+      	printing: './src/pages/printing/main.js'
   }, 
   publicPath: 
     process.env.NODE_ENV === 'production'
