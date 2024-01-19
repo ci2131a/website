426d3495638a2b6267dab9251cac4af0c496cac2
Charles Ingulli <charlesfi@outlook.com>
2024-01-18 23:50:29 -0500

    update commonent usage

---
+++ src/components/footer.vue
@@ -0,0 +1,16 @@
+<template>
+ <footer>
+ &copy; Copyright 2024 <a href = "https://ci2131a.github.io/mywebsite/" style = "text-decoration: none; color: inherit;"> Charles </a> Ingulli
+ </footer>
+</template>
+
+<style scoped>
+
+footer
+{
+display: flex;
+text-align: center; 
+height: 50px;
+width: 100%;}
+
+</style>
